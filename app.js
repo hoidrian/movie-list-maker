bc07a643a930fe53b66c21d4166c4db4893b8ebd
Adrian Yearwood <adrian.p.yearwood@gmail.com>
2023-01-06 11:18:11 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// Pseudocode
+
+// make namespace object for the movie
+    // make init function
+
+// make API request to get a random movie data
+    // title
+    // poster
+        // alt text
+    // description + run-time
+
+// make a container with 3 elements - h2 for movie title, img for poster, article for movie description
+
+// display the container in the middle of the screen
+
+// add event listener to the buttons
+    // right button click stores the object in a list and displays it below in a to-watch list 
+        // adds object as a list item to blank ul at bottom of page
+            // starts hidden, can be displayed via hover/click (CSS)
+            // thumbnail of poster, title, description + run-time
+    // left button click gets rid of the object and cycles to the next random movie
